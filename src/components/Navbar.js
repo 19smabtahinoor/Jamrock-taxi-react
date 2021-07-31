d4ce3d688a325a26b607150ff75d8141290343cb
19smabtahinoor <abtahinookabid@gmail.com>
2021-07-31 19:47:53 +0600

    Navbar Problem Done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
                         <NavLink className="navbar-brand me-5" to="/">
                             <img src="../../assets/logo.jpg" alt="logo" width="150px" height="100px" />
                         </NavLink>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
 
